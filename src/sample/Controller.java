932488f26e2388b54e8281b9b6ec03718a0bb8d2
Claudio <cclaudiohenriquez@gmail.com>
2020-08-07 18:38:05 -0400

    dadsad

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,10 +1,35 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 
 public class Controller {
+    @FXML
+    private Label Manuscrito;
+    @FXML
+    private TextField texto;
+
+
+
     public void bot(ActionEvent event){
         System.out.println("tetera");
+    }
+    public void rotar(){
+
+        try {
+            System.out.println(texto.getCharacters());
+            int angulo = Integer.parseInt(String.valueOf(texto.getCharacters()));
+            Manuscrito.setRotate(angulo);
+
+        }catch (NumberFormatException y){
+            System.out.println("no es valido la rotacion puesta");
+
+        }finally{
+
+        }
     }
 
 
